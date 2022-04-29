3259b67a050309b4655d3bb1bad00ba7dc00ff2f
Ajay Chauhan <ajay9670chauhan@gmail.com>
2022-04-30 00:51:26 +0530

    done with text field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Card, Container } from "react-bootstrap";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function App(props) {
+  const KeyBoardEvent = (event) =>{
+      event.persist();
+      if (event.key === "Enter") {
+        console.log('Enter ho gya ');
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Card className="text-center">
+          <Card.Header><h3>TodoList</h3></Card.Header>
+          <Card.Body>
+            <input type="text" className="mb-3" onKeyPress={KeyBoardEvent} />
+            {/* <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text> */}
+          </Card.Body>
+          <Card.Footer className="text-muted">Made by Ajay Chauhan</Card.Footer>
+        </Card>
+      </Container>
+    </>
   );
 }
 
